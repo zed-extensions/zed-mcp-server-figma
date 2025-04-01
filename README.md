31729f8cf6b91a641a542f13a5620946bf778a02
Jeffrey Guenther <jeff@loamstudios.com>
2025-04-01 07:39:30 -0700

    feat: create extension

---
+++ README.md
@@ -0,0 +1,17 @@
+# Figma MCP Server Extension for Zed
+
+This extension integrates [Framelink Figma MCP Server](https://github.com/GLips/Figma-Context-MCP)
+as a context server for Zed's Assistant.
+
+You'll need to grab a Figma access token for your account.
+Set the permissions `read-only` for _File Content_ and _Dev Resources_.
+
+```
+"context_servers": {
+    "mcp-server-figma": {
+      "settings": {
+        "figma_api_key": "<FIGMA_API_KEY>"
+      }
+    }
+  },
+```
---
+++ src/mcp_server_figma.rs
@@ -0,0 +1,55 @@
+use serde::Deserialize;
+use std::env;
+use zed::settings::ContextServerSettings;
+use zed_extension_api::{self as zed, serde_json, Command, ContextServerId, Project, Result};
+
+const PACKAGE_NAME: &str = "figma-developer-mcp";
+const SERVER_PATH: &str = "node_modules/figma-developer-mcp/dist/index.js";
+
+#[derive(Debug, Deserialize)]
+struct FigmaContextServerSettings {
+    figma_api_key: String,
+}
+
+struct FigmaModelContextExtension;
+
+impl zed::Extension for FigmaModelContextExtension {
+    fn new() -> Self {
+        Self
+    }
+
+    fn context_server_command(
+        &mut self,
+        _context_server_id: &ContextServerId,
+        project: &Project,
+    ) -> Result<Command> {
+        let latest_version = zed::npm_package_latest_version(PACKAGE_NAME)?;
+        let version = zed::npm_package_installed_version(PACKAGE_NAME)?;
+        if version.as_deref() != Some(latest_version.as_ref()) {
+            zed::npm_install_package(PACKAGE_NAME, &latest_version)?;
+        }
+
+        let settings = ContextServerSettings::for_project("mcp-server-figma", project)?;
+        let Some(settings) = settings.settings else {
+            return Err("missing `figma_api_key` setting".into());
+        };
+        let settings: FigmaContextServerSettings =
+            serde_json::from_value(settings).map_err(|e| e.to_string())?;
+
+        Ok(Command {
+            command: zed::node_binary_path()?,
+            args: vec![
+                env::current_dir()
+                    .unwrap()
+                    .join(SERVER_PATH)
+                    .to_string_lossy()
+                    .to_string(),
+                format!("--figma-api-key={}", settings.figma_api_key),
+                "--stdio".to_string(),
+            ],
+            env: Default::default(),
+        })
+    }
+}
+
+zed::register_extension!(FigmaModelContextExtension);
